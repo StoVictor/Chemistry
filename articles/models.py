311016a029886fcf5bb8269cea60e0f2d616acd1
StoVictor <1vitek99@gmail.com>
2018-03-13 21:47:28 +0100

    Initialize models

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
-from django.db import models
-
+from django.db import models 
 # Create your models here.
+
+class Topic():
+    pass
+
+
+class Exercise(models.Model):
+    pass
+
+
+class Theory(models.Model):
+    pass
+
+
+class Scientist(models.Model):
+    pass
