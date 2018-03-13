a03796bdd8d0fe1eb588fbd60b68e089b5a5401d
StoVictor <1vitek99@gmail.com>
2018-03-13 22:09:58 +0100

    Add a ckedtior to Article

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from treebeard.mp_tree import MP_Node
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Topic(MP_Node):
@@ -13,7 +14,11 @@ class Article(models.Model):
     name = models.CharField(max_length=200)
     hard = models.IntegerField()
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    importancy = models.CharField(max_length=100)    
+    importancy = models.CharField(max_length=100)
+    text = RichTextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Scientist(models.Model):
