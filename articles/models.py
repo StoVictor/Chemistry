16aa435497624816b3e64e2050502cea1424a92a
StoVictor <1vitek99@gmail.com>
2018-03-13 21:52:20 +0100

    develop a base of Topic model

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,13 @@
-from django.db import models 
+from django.db import models
+from treebeard.mp_tree import MP_Node
 # Create your models here.
 
-class Topic():
-    pass
+class Topic(MP_Node):
+    name = models.CharField(max_length=100)
+    node_order_by = ['name']
+
+    def __str__(self):
+        return 'Topic: %s' % self.name
 
 
 class Exercise(models.Model):
