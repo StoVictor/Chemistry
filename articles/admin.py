7f618012bc6c0657b9a0f24e0d5c1974fad3e3e6
StoVictor <1vitek99@gmail.com>
2018-03-13 22:15:45 +0100

    Add admin interface

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,20 @@
+from django import forms
+
 from django.contrib import admin
+from .models import Topic, Article, Scientist
+
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
+from ckeditor.widgets import CKEditorWidget
 
 # Register your models here.
+class MyAdmin(TreeAdmin):
+    form = movenodeform_factory(Topic)
+
+
+
+admin.site.register(Topic, MyAdmin)
+admin.site.register(Article)
+
+admin.site.register(Scientist)
