e14abd93371a5ddbd80d2d71b98ed4f49f3e1238
StoVictor <1vitek99@gmail.com>
2018-03-13 21:57:34 +0100

    Remove redunduncy models Theory and Exercise, make a new Articele model

--- articles/models.py
+++ articles/models.py
@@ -9,14 +9,13 @@ class Topic(MP_Node):
     def __str__(self):
         return 'Topic: %s' % self.name
 
-
-class Exercise(models.Model):
-    pass
-
-
-class Theory(models.Model):
-    pass
+class Article(models.Model):
+    name = models.CharField(max_length=200)
+    hard = models.IntegerField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    importancy = models.CharField(max_length=100)    
 
 
 class Scientist(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    year = models.CharField(max_length=20)
